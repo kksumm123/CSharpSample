103dd9ad4689fdcd9910c3ea1ae3ebad32b5eec6
hktrollgames <hktrollgames@gmail.com>
2021-05-05 21:15:24 +0900

    C#기본 테스트 문제 추가중

--- 0.레벨테스트문제/Program.cs
+++ 0.레벨테스트문제/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Text;
 
 namespace _0.레벨테스트문제
@@ -30,9 +31,49 @@ namespace _0.레벨테스트문제
             Sunday
         }
 
+        //문제 6에서 사용.
+        private struct ValueType
+        {
+            public int x;
+        }
+
+        private class RefType
+        {
+            public int x;
+        }
+
         private static void Main(string[] args)
         {
             // https://forms.gle/HNBT2KP2N94dJMTR6 // 실력 확인을 위한 설문조사
+
+            // 변수 생명 주기 : 선언된 괄호 안에서 살아 있다
+            {
+                int x = 0; // 변수(x)는 괄호가 끝나기 전까지 살아 있다.
+            }
+
+            {
+                // 선언되기 전에 사용할순 없다.
+                //Console.Write(x);
+
+                int x = 0; // 위에 있는 x와 변수 이름이 같지만 에러가 아니다. 괄호 내에 살아 있는 x가 없기 때문이다.
+            }// 괄호가 닫힐때 사라진다.
+
+            {
+                //함수사용법.
+                Method1(1, 2);
+                Method1(1, 2, 3);
+                Method1(param2: 1, param1: 2);
+            }
+
+            {
+                //함수 사용법 2 : 반환하고 싶은게 2개 이상일때?
+                // 방법1:반환하기 위한 임시 클래스(구조체 생성)
+                // 방법2: in, out파라미터 사용
+                int int1 = 1;
+                int int2 = 2;
+
+                Method2(in int1, out int2);
+            }
 
             // 문제1
             {
@@ -78,6 +119,30 @@ namespace _0.레벨테스트문제
                 CountCorrectAnswer("여기답적기", answer5);
             }
 
+            // 문제 6 값타입에서의 대입과 참조타입에서의 대입.
+            {
+                ValueType v1, v2;
+                v1.x = 1;
+                v2 = v1;
+                CountCorrectAnswer("여기답적기", v2.x);
+
+                RefType f1 = new RefType(), f2 = new RefType();
+                f1.x = 1;
+                f2 = f1;
+                CountCorrectAnswer("여기답적기", f2.x);
+            }
+
+            // 문제 7. 함수 기본값 사용
+            {
+                Point a = new Point(10, 20);
+                Point b = new Point3D(10, 20, 30); // 부모 클래스는 자식 클래스를 담을 수 있다.(자식 클래스에 있는 변수는 사용할 수 없다.
+                Point3D c = (Point3D)b;
+                CountCorrectAnswer("여기답적기", c.z);
+            }
+
+            {
+            }
+
             // 문자열 포멧 사용.
             {
                 int score = 1000;
@@ -86,24 +151,98 @@ namespace _0.레벨테스트문제
                 Console.WriteLine(string.Format("{0:N}", score));
             }
 
+            // static을 이해하고 있는가?
+            // static(정적) <-> dynamic(동적)
+            // static 프로그램 실행시바로 초기화 되어 언제든지 사용할 수 있다
+            // dynamic은 프로그램 실행중에 코드가 실행되면 초기화되어 사용된다
+            {
+                StaticVariableInclude staticVariableInclude = new StaticVariableInclude();
+                staticVariableInclude.normalInt = 1;
+                StaticVariableInclude.staticInt = 2;
+            }
+
+            {
+                StaticVariableInclude staticVariableInclude = new StaticVariableInclude();
+
+                CountCorrectAnswer("여기답적기", staticVariableInclude.normalInt, "일반 변수 생존 주기");
+                CountCorrectAnswer("여기답적기", StaticVariableInclude.staticInt, "Static 변수 생존 주기");
+            }
+
             // 확장 함수 사용
             {
                 int score = 1000;
                 Console.WriteLine(score.ToNumber());
             }
 
+            // 형식 매개 변수
+            {
+                //List는 어떻게 모든 타입을 사용할 수 있을까?
+                List<int> intList = new List<int>();
+                List<string> stringList = new List<string>();
+
+                // 우리도 모든 타입을 담는 클래스를 만들어보자.
+                AllType<int> intVariable = new AllType<int>();
+                intVariable.value = 1;
+
+                AllType<string> stringVariable = new AllType<string>();
+                stringVariable.value = "string";
+            }
+
             Console.WriteLine($"맞힌 정답수는 : {correctAnswer}개({correctAnswer / (float)qustionCount * 100})%입니다");
             Console.WriteLine(sb.ToString());
         }
 
+        private static void Method1(int param1, int param2, int param3 = 10)
+        {
+            Console.WriteLine($"parma1:{param1}, parma2:{param2}, parma3:{param3}");
+        }
+
+        private static void Method2(in int param1, out int param2)
+        {
+            param2 = 3; // out 파라미터는
+        }
+
+        private class StaticVariableInclude
+        {
+            static public int staticInt;
+            public int normalInt;
+        }
+
+        public class AllType<UndefinedType>
+        {
+            public UndefinedType value;
+        }
+
+        public class Point
+        {
+            public int x, y;
+
+            public Point(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+        }
+
+        public class Point3D : Point
+        {
+            public int z;
+
+            public Point3D(int x, int y, int z) : base(x, y)
+            {
+                this.z = z;
+            }
+        }
+
         private static StringBuilder sb = new StringBuilder();
 
-        private static void CountCorrectAnswer(string 입력한답, string answer)
+        private static void CountCorrectAnswer(string 입력한답, Object answerObject, string 문제내용 = "")
         {
             qustionCount++;
 
+            string answer = answerObject.ToString();
             입력한답 = 입력한답.Replace(" ", "").Replace("여기답적기", "").ToLower();
-            answer = answer.Replace(" ", "").ToLower();
+            answer = answer.ToString().Replace(" ", "").ToLower();
 
             bool isCorrect = false;
             if (입력한답 == answer)
@@ -111,7 +250,7 @@ namespace _0.레벨테스트문제
 
             if (isCorrect)
                 correctAnswer++;
-            sb.AppendLine($"문제{qustionCount}:{(isCorrect ? "o" : "x")}\n\t입력:{입력한답}\n\t정답:{answer}\n");
+            sb.AppendLine($"문제{qustionCount}:{(isCorrect ? "o" : "x")} {문제내용}\n\t입력:{입력한답}\n\t정답:{answer}\n");
         }
     }
 }

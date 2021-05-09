59ccd6eb2d53411af3563291a4b3686880e7389d
hktrollgames <hktrollgames@gmail.com>
2021-05-10 08:53:03 +0900

    머드 게임 추가

---
+++ 머드게임/Monster.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace 머드게임
+{
+
+    internal class Monster
+    {
+        static Random random = new Random();
+        static int idTotal;
+
+        public int id;
+        public string name;
+        public int power;
+        public int hp;
+        public Monster()
+        {
+            id = ++idTotal;
+
+            name = "몬스터" + id;
+            power = random.Next(1, 2);
+            hp = random.Next(1, 2);
+        }
+    }
+}

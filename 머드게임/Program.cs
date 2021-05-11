b18574a3baeec27e898a584d4471b63ac9de0a4c
chanseon park <hktrollgames@gmail.com>
2021-05-11 10:50:09 +0900

    기능별로 함수 분리

--- 머드게임/Program.cs
+++ 머드게임/Program.cs
@@ -15,98 +15,133 @@ namespace 머드게임
             bool quit = false;
             do
             {
-                //quit = true; // <- 나가야 되는 상황에서 변경
-                Print("당신의 이름을 입력해주세요");
-                string userName = Console.ReadLine();
-
-                bool isReset = true;
-                int power = 0, hp = 0;
-
-                while (isReset)
-                {
-                    power = random.Next(4, 10);// 4는 포함하고 10은 포함하지 않는 -> 4 ~ 9
-                    hp = random.Next(3, 10);
-                    //당신의 능력은 입니다. 공격력 3, 체력 5 입니다.
-                    Print($"{userName}님은 공격력:{power}, 체력:{hp} 입니다. 다시 생성 하시겠습니까?(Y/N)");
-                    string resetAnswer = Console.ReadLine(); //  y, n
-                    isReset = resetAnswer.ToLower() == "y";
-                };
-
-                Player player = new Player(userName, power, hp);
+                // 플레이어 이름 입력받기, 능력치 랜덤 설정.
+                Player player = MakePlayer();
 
                 Print("");
                 Print("탐험을 시작합니다. Press any key");
+                Console.ReadKey();  // 키 입력 받기
 
-                // 키 입력 받기
-                Console.ReadKey();
+                quit = ProcessBattle(player);
 
-                #region 던전 들어가기 시작
+            } while (quit == false);
+        }
 
-                // 몬스터 스폰, 리젠
-                int monsterCount = random.Next(1, 3); // 1 ~ 2개(최소값은 포함해도 최대값은 포함하지 않음) 
-                List<Monster> monsters = new List<Monster>();
-                for (int i = 0; i < monsterCount; i++)
-                {
-                    monsters.Add(new Monster());
-                }
+        private static Player MakePlayer()
+        {
 
-                Print($"몬스터 {monsterCount}마리를 만났습니다.");
+            //quit = true; // <- 나가야 되는 상황에서 변경
+            Print("당신의 이름을 입력해주세요");
+            string userName = Console.ReadLine();
 
-                // 문자열앞에 $와@를 사용하는 예제
-                // $ : "몬스터 {2}마리를 만났습니다. 전투 시작!")
-                //  : "몬스터 {monsterCount}마리를 만났습니다. 전투 시작!")
-                string s1 = @"1
+            bool isReset = true;
+            int power = 0, hp = 0;
+
+            while (isReset)
+            {
+                power = random.Next(4, 10);// 4는 포함하고 10은 포함하지 않는 -> 4 ~ 9
+                hp = random.Next(3, 10);
+                //당신의 능력은 입니다. 공격력 3, 체력 5 입니다.
+                Print($"{userName}님은 공격력:{power}, 체력:{hp} 입니다. 다시 생성 하시겠습니까?(Y/N)");
+                string resetAnswer = Console.ReadLine(); //  y, n
+                isReset = resetAnswer.ToLower() == "y";
+            };
+
+            Player player = new Player(userName, power, hp);
+            return player;
+        }
+
+        private static bool ProcessBattle(Player player)
+        {
+            // 몬스터 스폰, 리젠
+            int monsterCount = random.Next(1, 3); // 1 ~ 2개(최소값은 포함해도 최대값은 포함하지 않음) 
+            List<Monster> monsters = new List<Monster>();
+            for (int i = 0; i < monsterCount; i++)
+            {
+                monsters.Add(new Monster());
+            }
+
+            Print($"몬스터 {monsterCount}마리를 만났습니다.");
+
+            // 문자열앞에 $를 사용하는 예제
+            // $를 문자열앞에 붙였을때        : "몬스터 {2}마리를 만났습니다.")
+            // $를 문자열앞에 붙이지 않았을때 : "몬스터 {monsterCount}마리를 만났습니다.")
+
+            // 문자열앞에 @를 붙였을때의 예제
+            // @를 붙이면 줄바꿈과 빈공간 표시를 직관적으로 할수 있다.
+            string s1 = @"1
 줄바꿈
                     빈공간";
 
-                string s2 = "1\n줄바꿈\n" +
+            //문자여에서 글자 앞에 '\'이 있으면 이스케이프 문자를 의미합니다
+            // 이스케이프 문자는 뒤에 오는 글자에 따라 다양한 기능이 있습니다.
+            // 예를 들어 \n은 줄바꿈 표시 입니다.
+            // https://docs.microsoft.com/ko-kr/dotnet/standard/base-types/character-escapes-in-regular-expressions
+            string s2 = "1\n줄바꿈\n" +
 "                   빈공간";
 
-                while (player.hp > 0)
+            // $와 @는 동시에 사용가능하며 순서는 상관없다.
+
+            while (player.hp > 0)
+            {
+                Print("");
+                Print("몬스터 정보 출력");
+                // 몬스터 정보 출력.
+                foreach (var m in monsters)
                 {
-                    Print("");
-                    Print("몬스터 정보 출력");
-                    // 몬스터 정보 출력.
-                    foreach (var m in monsters)
-                    {
-                        Print($"{m.name} 공격력:{m.power}, 체력:{m.hp}");
-                    }
-
-                    // 유저 정보 출력.
-                    Print($"{player.userName} 공격력:{player.power}, 체력:{player.hp}");
-
-                    Print("");
-                    //유저 행동.
-                    Print("1:공격, 2:회복, 3:도망");  //\n 이스케이프 문자.
-                    char userInput = Console.ReadKey().KeyChar; // '1' ->  "1"
-
-                    //string userName = Console.ReadLine();// <- 기존에 유저 입력 받던 방식.
-
-                    switch (userInput)
-                    {
-                        case '1':// 공격
-                            PlayerAttack(player, monsters);
-                            break;
-                        case '2': // 회복
-                            player.RestoreHp();
-                            break;
-                        case '3': // 도망.
-                            bool successRun = TryRun();
-                            break;
-                        default:
-                            Print("없는 명령어 입니다" + userInput);
-                            break;
-                    }
-
-                    // 몬스터가 플레이어를 공겨.
-                    MonsterAttackToPlayer(player, monsters);
+                    Print($"{m.name} 공격력:{m.power}, 체력:{m.hp}");
                 }
 
-                Print(@$"{userName}은 사망했습니다.
-GameOver");
+                // 유저 정보 출력.
+                Print($"{player.userName} 공격력:{player.power}, 체력:{player.hp}");
 
-                #endregion 던전 전투 끝
-            } while (quit == false);
+                //유저 행동.
+                PlayerTurn(player, monsters);
+
+                // 몬스터가 플레이어를 공격.
+                MonsterTurn(player, monsters);
+            }
+
+            Print(@$"{player.userName}은 사망했습니다.
+GameOver
+처음부터 하시겠습니까?(R)etry/(Q)uit");
+
+            string retryOrQuit = Console.ReadKey().KeyChar.ToString().ToUpper();
+            bool isQuit = retryOrQuit == "Q";
+            return isQuit;
+        }
+
+        private static void MonsterTurn(Player player, List<Monster> monsters)
+        {
+            // todo: 몬스터 HP회복 진행.
+
+            // 몬스터가 플레이어 때리는 과정 진행
+            MonsterAttackToPlayer(player, monsters);
+        }
+
+        private static void PlayerTurn(Player player, List<Monster> monsters)
+        {
+            Print("");
+            Print("1:공격, 2:회복, 3:도망");
+            char userInput = Console.ReadKey().KeyChar; // '1' ->  "1"
+
+            //string userName = Console.ReadLine();// <- 기존에 유저 입력 받던 방식.
+
+            switch (userInput)
+            {
+                case '1':// 공격
+                    PlayerAttack(player, monsters);
+                    break;
+                case '2': // 회복
+                    player.RestoreHp();
+                    break;
+                case '3': // 도망.
+                    bool successRun = TryRun();
+                    break;
+                default:
+                    Print("없는 명령어 입니다" + userInput);
+                    break;
+            }
         }
 
         private static void Print(string log)
@@ -116,7 +151,8 @@ GameOver");
 
         private static void MonsterAttackToPlayer(Player player, List<Monster> monsters)
         {
-            Print("몬스터가 플레이어를 공격하는 로직은 아직 작성하지 않음");
+            Print("모든 몬스터가 플레이어 공격");
+
         }
 
         private static void PlayerAttack(Player player, List<Monster> monsters)

90ca4e64e3da290e845ed48ed166c709c8c980aa
chanseon park <hktrollgames@gmail.com>
2021-05-13 09:40:40 +0900

    Virtual 함수와 Interface 사용법

---
+++ Virtual 함수 사용법/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Virtual_함수_사용법
+{
+    class ChildMonsterA 
+    {
+        public void Attack(Player player)
+        {
+            Console.WriteLine("A Attack");
+        }
+    }
+    class ChildMonsterB 
+    {
+        public void Attack(Player player)
+        {
+            Console.WriteLine("B Attack");
+        }
+    }
+
+    class ChildMonsterC 
+    {
+        public void Attack(Player player)
+        {
+            Console.WriteLine("C Attack");
+        }
+    }
+
+    class Player
+    {
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("virtual 사용법 - 상속없이 구현");
+
+            Player player = new Player();
+
+            ChildMonsterA a1 = new ChildMonsterA();
+            ChildMonsterA a2 = new ChildMonsterA();
+            ChildMonsterA a3 = new ChildMonsterA();
+
+            ChildMonsterB b1 = new ChildMonsterB();
+            ChildMonsterB b2 = new ChildMonsterB();
+
+            ChildMonsterC c1 = new ChildMonsterC();
+
+            List<ChildMonsterA> aMonsters = new List<ChildMonsterA> { a1, a2, a3 };
+            List<ChildMonsterB> bMonsters = new List<ChildMonsterB> { b1, b2 };
+            List<ChildMonsterC> cMonsters = new List<ChildMonsterC> { c1 };
+
+            aMonsters.ForEach(x => x.Attack(player));
+            bMonsters.ForEach(x => x.Attack(player));
+            cMonsters.ForEach(x => x.Attack(player));
+        }
+    }
+}
---
+++ Virtual 함수 사용법_인터페이스로 구현/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Virtual_함수_사용법_인터페이스로_구현
+{
+    public interface IAttack
+    {
+        public void Attack(Player player);
+    }
+
+    class Monster
+    {
+    }
+
+    class ChildMonsterA : Monster, IAttack // 클래스는 1개만 상속 가능하지만 인터페이스는 갯수 제한 없음
+    {
+        public void Attack(Player player)
+        {
+            Console.WriteLine("A Attack");
+        }
+    }
+    class ChildMonsterB : Monster, IAttack
+    {
+        public void Attack(Player player)
+        {
+            Console.WriteLine("B Attack");
+        }
+    }
+
+    class ChildMonsterC : Monster, IAttack
+    {
+        public void Attack(Player player)
+        {
+            Console.WriteLine("C Attack");
+        }
+    }
+
+    class Trap : IAttack
+    {
+        public void Attack(Player player)
+        {
+            Console.WriteLine("Trap Attack");
+        }
+    }
+
+    public class Player
+    {
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("virtual 사용법 - 상속으로 구현");
+
+            Player player = new Player();
+
+            ChildMonsterA a1 = new ChildMonsterA();
+            ChildMonsterA a2 = new ChildMonsterA();
+            ChildMonsterA a3 = new ChildMonsterA();
+
+            ChildMonsterB b1 = new ChildMonsterB();
+            ChildMonsterB b2 = new ChildMonsterB();
+
+            ChildMonsterC c1 = new ChildMonsterC();
+
+            Trap t1 = new Trap();
+
+            List<IAttack> allMonsters = new List<IAttack> { a1, a2, a3, b1, b2, c1, t1 };
+
+            allMonsters.ForEach(x => x.Attack(player));
+        }
+    }
+}
